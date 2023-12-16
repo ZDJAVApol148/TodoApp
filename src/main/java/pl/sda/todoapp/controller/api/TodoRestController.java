b07c693f2271bde65ce1bd5d279c531f786b904c
Marcin Szupke <marcin.szupke@gmail.com>
2023-12-16 12:42:06 +0100

    todo rest controller

---
+++ src/main/java/pl/sda/todoapp/controller/api/TodoRestController.java
@@ -0,0 +1,35 @@
+package pl.sda.todoapp.controller.api;
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pl.sda.todoapp.model.dto.TodoDto;
+import pl.sda.todoapp.service.TodoService;
+
+import java.util.List;
+
+@RestController
+public class TodoRestController {
+
+    private final TodoService todoService;
+
+    public TodoRestController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping("/todo")
+    public List<TodoDto> getList() {
+        List<TodoDto> todos = todoService.getList();
+        return todos;
+    }
+
+    @PostMapping("/todo")
+    public ResponseEntity<String> create(@RequestBody TodoDto todo) {
+        todoService.saveOrUpdate(todo);
+
+        return ResponseEntity.status(HttpStatusCode.valueOf(201)).body("OK");
+    }
+}
